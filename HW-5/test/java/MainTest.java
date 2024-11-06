b58beddce0bfd0d7419be2a09d632b468ebf23c2
MdAbrarFahim <106899513+MdAbrarFahim@users.noreply.github.com>
2024-11-06 10:23:11 -0800

    Delete HW-5/test/java directory

--- HW-5/test/java/MainTest.java
+++
@@ -1,9 +0,0 @@
-//import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Test;
-public class MainTest {
-
-    @Test
-    public void testMainExecution() {
-        Main.main(new String[]{});
-    }
-}
